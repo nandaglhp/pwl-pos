3eee4e8d48dd498c4411b9dc0948f656d61adb92
Ananda Galih Pratiwi <nandaglhp@gmail.com>
2024-08-16 09:30:57 +0700

    pertemuan-23

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\WelcomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,4 @@ Route::get('/', function () {
 
 Route::get('level', [LevelController::class,'index']);
 Route::get('kategori', [KategoriController::class,'index']);
+Route::get('/', [WelcomeController::class,'index']);
